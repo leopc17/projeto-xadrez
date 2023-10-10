14f9dc34e78a7f4fc29cc2fe18c06c30faf15d50
leopc17 <LeonardoEAF45@gmail.com>
2023-10-10 10:09:22 -0300

    projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position p = new Position(5, 4);
+        System.out.println(p);
+    }
+}
