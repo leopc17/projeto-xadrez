897f5c211bc2591b69795a9436ae1ca20c4cda55
leopc17 <LeonardoEAF45@gmail.com>
2023-10-10 22:15:40 -0300

    novas classes e criação da interface inicial

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,11 @@
 package application;
 
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
-        Position p = new Position(5, 4);
-        System.out.println(p);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
